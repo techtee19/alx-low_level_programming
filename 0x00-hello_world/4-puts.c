186f2fad51668a2f597de1d8c0c94a511b25be1f
techtee19 <techtee19@gmail.com>
2023-02-20 06:20:38 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *  *main - entry point
- *   *
- *    *Return: 0 after printing the function     
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
