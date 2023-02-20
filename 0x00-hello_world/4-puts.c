f8b52f9bce3e231f0eec5b892546998700a70d83
techtee19 <techtee19@gmail.com>
2023-02-20 05:43:41 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * *
+ *  *Return: 0 after ptinting this function
+ *   */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzl");
+  return(0);
+}
