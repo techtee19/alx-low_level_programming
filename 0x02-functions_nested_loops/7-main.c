d820930f0142909c197c59bbe1976c6b0ffcc3c5
techtee19 <techtee19@gmail.com>
2023-02-22 11:04:54 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -1,27 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 
-
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
     int r;
 
-    r = print_last_digit(98);
     print_last_digit(98);
-    putchar('0' + r);
-    putchar('\n');
-    return (0);
-    r = print_last_digit(0);
     print_last_digit(0);
-    putchar('0' + r);
-    putchar('\n');
-    return (0);
-    print_last_digit(-1024);
     r = print_last_digit(-1024);
     putchar('0' + r);
     putchar('\n');
