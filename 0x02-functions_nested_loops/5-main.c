6f5ea84fee979cda6ca1e332b85b8e1459c9620e
techtee19 <techtee19@gmail.com>
2023-02-22 06:35:11 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -15,20 +15,24 @@ int main(void)
     putchar(' ');
     putchar(r + '0');
     putchar('\n');
+
     r = print_sign(0);
     putchar(',');
     putchar(' ');
     putchar(r + '0');
     putchar('\n');
+
     r = print_sign(0xff);
     putchar(',');
     putchar(' ');
     putchar(r + '0');
     putchar('\n');
+
     r = print_sign(-1);
     putchar(',');
     putchar(' ');
     putchar(r + '0');
     putchar('\n');
+
     return (0);
 }
