66c9fd8d4260920a2610a9ea83255d2fd956d773
techtee19 <techtee19@gmail.com>
2023-02-22 11:50:22 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,11 +11,16 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	last_digit = n % 10;
+	if (n < 0)
+	{
+		last_digit = -(n % 10);
+	}
+	else
+	{
+		last_digit = n % 10;
+	}
 
-	if (last_digit < 0)
-		last_digit = -last_digit;
-	putchar('0' + last_digit);
+	putchar(last_digit + '0');
 
 	return (last_digit);
 }
