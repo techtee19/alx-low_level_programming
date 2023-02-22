0c4bdb0675cabf7b191ea4c381ef38972c7a77b7
techtee19 <techtee19@gmail.com>
2023-02-22 07:39:47 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -11,8 +11,17 @@ int main(void)
 {
     int r;
 
+    r = print_last_digit(98);
     print_last_digit(98);
+    putchar('0' + r);
+    putchar('\n');
+    return (0);
+    r = print_last_digit(0);
     print_last_digit(0);
+    putchar('0' + r);
+    putchar('\n');
+    return (0);
+    print_last_digit(-1024);
     r = print_last_digit(-1024);
     putchar('0' + r);
     putchar('\n');
